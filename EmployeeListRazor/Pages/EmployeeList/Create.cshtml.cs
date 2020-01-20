25ea00ef62dc5e6e23b6ad85939aa5f7eb387320
shihab <echoshihab@gmail.com>
2020-01-19 20:55:48 -0500

    add create functionality with validations

--- EmployeeListRazor/Pages/EmployeeList/Create.cshtml.cs
+++ EmployeeListRazor/Pages/EmployeeList/Create.cshtml.cs
@@ -17,10 +17,28 @@ namespace EmployeeListRazor
             _db = db;
         }
 
+        [BindProperty] //this binds the employee object below to post so that you don't have to type onPost(Employee employeeOBJ)
         public Employee Employee { get; set; }
         public void OnGet()
         {
 
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Employee.AddAsync(Employee);
+                await _db.SaveChangesAsync(); //this pushes changes to database
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
     }
+
+
 }
