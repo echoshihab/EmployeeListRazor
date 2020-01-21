063c8573775c19b279afd84ee1f3c1912dadbb42
shihab <echoshihab@gmail.com>
2020-01-20 20:56:21 -0500

    add style and create upsert page

--- EmployeeListRazor/Controllers/EmployeeController.cs
+++ EmployeeListRazor/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using EmployeeListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeListRazor.Controllers
 {
@@ -19,9 +20,24 @@ namespace EmployeeListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Employee.ToList() });
+            return Json(new { data =await _db.Employee.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var employeeFromDb = await _db.Employee.FirstOrDefaultAsync(u => u.Id == id);
+
+            if(employeeFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+
+            }
+            _db.Employee.Remove(employeeFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
         }
     }
 }
---
+++ EmployeeListRazor/Pages/EmployeeList/upsert.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeListRazor
+{
+    public class UpsertModel : PageModel
+    {
+
+        private ApplicationDbContext _db;
+
+        public UpsertModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Employee Employee { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+  
+            Employee = new Employee();
+            if(id == null)
+            {
+                //create
+                return Page();
+            }
+            Employee = await _db.Employee.FirstOrDefaultAsync(u => u.Id == id);
+
+            if(Employee == null)
+            {
+                //update
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPost()  //using iactionresult because of redirection
+        {
+            if (ModelState.IsValid)
+            {
+               if(Employee.Id == 0)
+                {
+                    _db.Employee.Add(Employee);
+
+                }
+               else
+                {
+                    _db.Employee.Update(Employee);
+                }
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
